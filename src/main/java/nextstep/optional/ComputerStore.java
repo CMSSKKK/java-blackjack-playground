7fe687e666e119fdec6304189a855d5a427a97b6
Minseok-Choi <cms05041@gmail.com>
2022-01-23 16:33:20 +0900

    optinal 연습

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,9 +3,10 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
     public static final String UNKNOWN_VERSION = "UNKNOWN";
-
     public static String getVersion(Computer computer) {
         String version = UNKNOWN_VERSION;
         if (computer != null) {
@@ -21,6 +22,11 @@ public class ComputerStore {
     }
 
     public static String getVersionOptional(Computer computer) {
-        return null;
+        Optional<Computer> optionalComputer = Optional.ofNullable(computer);
+        String version = optionalComputer.map(Computer::getSoundcard)
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion).orElse(UNKNOWN_VERSION);
+
+        return version;
     }
 }
